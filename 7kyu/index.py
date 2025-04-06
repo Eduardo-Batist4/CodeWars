0c70a12accb5c2ff829759b2f67d99a4bc3baf55
Eduardo-Batist4 <carloswe3st@gmail.com>
2025-04-06 13:34:12 -0300

    first commit

---
+++ 7kyu/index.py
@@ -0,0 +1,26 @@
+"""
+# (1) - Descending Order
+def descending_order(num):
+    for i in str(num):
+        print(i.sort(reversed=True))
+
+descending_order(42145)
+"""
+
+# -------------------------------------------------------------------------- #
+"""
+def get_middle(s):
+    tam = len(s)
+    if tam % 2 == 0:
+        return s[tam//2 - 1] + s[tam//2]
+    else:
+        return s[tam//2]
+        
+get_middle('test')
+get_middle('testing')
+get_middle('middle')
+get_middle('a')
+"""
+
+# -------------------------------------------------------------------------- #
+
