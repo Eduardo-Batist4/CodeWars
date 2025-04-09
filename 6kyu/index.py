e290a585626df2a414ff01923bd984fe96643a14
Eduardo-Batist4 <carloswe3st@gmail.com>
2025-04-09 19:03:27 -0300

    array.diff - 6kyu

--- 6kyu/index.py
+++ 6kyu/index.py
@@ -1,3 +1,4 @@
+"""
 def solution(number):
     totalSum = 0
     for i in range(2, number):
@@ -6,3 +7,35 @@ def solution(number):
     return totalSum
         
 solution(10)
+"""
+# -------------------------------------------------------------------------------------- #
+"""
+def likes(names):
+    if (len(names) == 0):
+        return "no one likes this"
+    elif (len(names) == 1):
+        return f"{names[0]} likes this"      
+    elif (len(names) == 2):
+        return f"{names[0]} and {names[1]} likes this"      
+    elif (len(names) == 3):
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+    else:
+        return f"{names[0]}, {names[1]} and {len(names) - 2} likes this"
+    
+likes([])
+likes(["Peter"])
+likes(["Max", "John"])
+likes(["Max", "John", "Mark"])
+likes(["Alex", "Jacob", "Mark", "Max", "ola"])
+"""
+# -------------------------------------------------------------------------------------- #
+def array_diff(a, b):
+    array = []
+    for i in a:
+        if (i not in b):
+            array.append(i)
+    return array
+
+
+array_diff([1,2], [1])
+array_diff([1,2,2,2,2,3], [2])
